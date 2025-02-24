997337367f8c5f6cc01e236764d14697b8977ed0
Dario Andres Baldiviezo Martinez <dbaldivi@made-f0Br8s10.clusters.42paris.fr>
2025-02-24 17:21:17 +0100

    c02 ex09 add a case test for capitalize

--- c02/ex09.c
+++ c02/ex09.c
@@ -8,6 +8,8 @@ int main()
 	int t1;
 	char str1[] = "salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
 	char *str2 = "Salut, Comment Tu Vas ? 42mots Quarante-Deux; Cinquante+Et+Un";
+	char str3[] = "*&^salut, tout va 10points";
+	char *str4 = "*&^Salut, Tout Va 10points";
 
 	ft_strcapitalize(str1);
 	t1 = strcmp(str1, str2);
@@ -15,6 +17,14 @@ int main()
 	{
 		printf("FAILED. Result/Expected\n");
 		printf("%s\n%s", str1, str2);
+		return (0);
+	}
+	ft_strcapitalize(str3);
+	t1 = strcmp(str4, str3);
+	if (t1)
+	{
+		printf("FAILED. Result/Expected\n");
+		printf("%s\n%s", str3, str4);
 		return (0);
 	}
 	printf("OK");
