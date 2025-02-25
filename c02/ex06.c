9198198e663edb7579932a3c67c26fc67f18fee0
Dario Andres Baldiviezo Martinez <dbaldivi@made-f0Br8s10.clusters.42paris.fr>
2025-02-25 11:22:21 +0100

    Fix error for c02 ex06

--- c02/ex06.c
+++ c02/ex06.c
@@ -5,26 +5,26 @@ int ft_str_is_printable(char *str);
 int main()
 {
 	int t1;
-	char *str1 = "acbdefg";
+	char str1[] = "abc-_-abc";
 
-	str1[2] = 127;
+	str1[4] =  '\0' + 127;
 	t1 = ft_str_is_printable(str1);
 	if (t1)
 	{
-		printf("FAILED. Result/Expected\n");
-		printf("%s: %d/%d\n", str1, 1, t1);
+		printf("FAILED. Result/value\n");
+		printf("%s: %d/%d\n", str1, t1, 127);
 		return (0);
 	}
 
 	int i = 0;
-	while (i++ < 127)
+	while (++i < 127)
 	{
-		str1[2] = '\0' + i;
+		str1[4] = '\0' + i;
 		t1 = ft_str_is_printable(str1);
-		if ((t1 && t1 < 32) || (!t1 && t1 > 31))
+		if ((t1 && i < 32) || (!t1 && i > 31))
 		{
-			printf("FAILED. Result/Expected\n");
-			printf("%s: %d/%d\n", str1, 1, t1);
+			printf("FAILED. Result/value\n");
+			printf("%s: %d/%d\n", str1, t1, i);
 			return (0);
 		}
 	}
